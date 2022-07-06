20101f386490654f983e1eb763dac4ced91edee3
nayanrdeveloper <nayanrdeveloper@gmail.com>
2022-07-06 09:16:13 +0530

    [MOD] add EXperience Level

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Intro from './components/Intro/Intro';
 import Navbar from './components/Navbar/Navbar';
 import Services from './components/Services/Services';
 import Cards from './components/Cards/Cards';
+import Experience from './components/Experience/Experience';
 
 
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
-     <Intro />
-     <Services />
+      <Navbar />
+      <Intro />
+      <Services />
+        <Experience />
     </div>
   );
 }
---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Experience.css'
+
+export default function experience(props) {
+  return (
+    <div className='experience'>
+      <div className='archievement'>
+        <span className='circle'>2+</span>
+        <span>Years</span>
+        <span>Experience</span>
+      </div>
+      <div className='archievement'>
+        <span className='circle'>2+</span>
+        <span>Years</span>
+        <span>Experience</span>
+      </div>
+      <div className='archievement'>
+        <span className='circle'>2+</span>
+        <span>Years</span>
+        <span>Experience</span>
+      </div>
+    </div>
+  )
+}
